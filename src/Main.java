4431e9f6ee01a60fceef5ec11634513592cd03e4
Alla-java <goneman@yandex.ru>
2024-10-05 11:05:16 +0300

    Создано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+       /* Задание 1. */
+        int [ ] payments = {180, 478, 284, 583, 983};
+        int total = 0;
+        for (int i: payments) {
+            total +=i;
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " руб");
+
+
+
+        /* Задание 2. */
+        int min = payments [0];
+        int max = payments [0];
+
+        for (int i: payments) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+
+        /* Задание 3. */
+        for (int y: payments) {
+            total +=y;
+        }
+
+        double average = total / payments.length;
+        System.out.println("Средняя сумма трат за день составила " + average + " рублей");
+
+        /* Задание 4. */
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length-1; i>=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
+
+
+
     }
 }
